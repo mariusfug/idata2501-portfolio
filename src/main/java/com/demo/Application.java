dee75d3736090a6fa4f812f84d8a360f3738d5f1
James Starmer <james@starmer.com>
2016-01-10 22:18:17 -0500

    Removed unused import

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -1,7 +1,5 @@
 package com.demo;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,4 +10,5 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
+
 }
