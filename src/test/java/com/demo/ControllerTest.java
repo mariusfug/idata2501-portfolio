dcdf275bab2dd0d4a6d2683a07df8fe32e061a75
mariufug <77608175+mariusfug@users.noreply.github.com>
2022-12-09 01:59:41 +0100

    Fixed test errors

--- src/test/java/com/demo/ControllerTest.java
+++ src/test/java/com/demo/ControllerTest.java
@@ -1,11 +1,13 @@
 package com.demo;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ControllerTest {
 
